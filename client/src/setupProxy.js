89c66c56b9c1a5e731f0ecf20ad65a7fa36cd06b
Ivan <53820994+Battlecry17@users.noreply.github.com>
2023-06-10 22:01:22 +0500

    Deploy changes

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:3001",
+      target: "https://ivan-chkalov-cv.onrender.com/api",
       changeOrigin: true,
     })
   );
